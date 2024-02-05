ae5ca3e89dd35a8896aab668440a6d13b06492ca
Etienne <e.desbois@hotmail.fr>
2024-02-05 22:00:34 +0100

    Bump wgpu to 0.19, egui to 0.26, web-sys 0.67

--- mesh_to_sdf_client/src/runner.rs
+++ mesh_to_sdf_client/src/runner.rs
@@ -1,4 +1,4 @@
-use egui_wgpu::renderer::{Renderer, ScreenDescriptor};
+use egui_wgpu::{Renderer, ScreenDescriptor};
 use std::sync::{Arc, Mutex};
 use winit::dpi::PhysicalSize;
 use winit::event::StartCause;
@@ -82,7 +82,7 @@ impl EventLoopWrapper {
 ///
 /// As surface usage varies per platform, wrapping this up cleans up the event loop code.
 struct SurfaceWrapper {
-    surface: Option<wgpu::Surface>,
+    surface: Option<wgpu::Surface<'static>>,
     config: Option<wgpu::SurfaceConfiguration>,
 }
 
@@ -104,9 +104,7 @@ impl SurfaceWrapper {
     /// us to wait until we recieve the `Resumed` event to do so.
     fn pre_adapter(&mut self, instance: &wgpu::Instance, window: Arc<Window>) {
         if cfg!(target_arch = "wasm32") {
-            unsafe {
-                self.surface = Some(instance.create_surface(&window).unwrap());
-            }
+            self.surface = Some(instance.create_surface(window).unwrap());
         }
     }
 
@@ -130,9 +128,7 @@ impl SurfaceWrapper {
 
         // We didn't create the surface in pre_adapter, so we need to do so now.
         if !cfg!(target_arch = "wasm32") {
-            unsafe {
-                self.surface = Some(context.instance.create_surface(&window).unwrap());
-            }
+            self.surface = Some(context.instance.create_surface(window).unwrap());
         }
 
         // From here on, self.surface should be Some.
@@ -228,7 +224,7 @@ impl ExampleContext {
     async fn init_async(surface: &mut SurfaceWrapper, window: Arc<Window>) -> Self {
         log::info!("Initializing wgpu...");
 
-        let backends = wgpu::util::backend_bits_from_env().unwrap_or(wgpu::Backends::all());
+        let backends = wgpu::util::backend_bits_from_env().unwrap_or_default();
         let dx12_shader_compiler = wgpu::util::dx12_shader_compiler_from_env().unwrap_or_default();
         let gles_minor_version = wgpu::util::gles_minor_version_from_env().unwrap_or_default();
 
@@ -280,8 +276,8 @@ impl ExampleContext {
             .request_device(
                 &wgpu::DeviceDescriptor {
                     label: Some("Device Descriptor"),
-                    features: (optional_features & adapter_features) | required_features,
-                    limits: needed_limits,
+                    required_features: (optional_features & adapter_features) | required_features,
+                    required_limits: needed_limits,
                 },
                 trace_dir.ok().as_ref().map(std::path::Path::new),
             )
