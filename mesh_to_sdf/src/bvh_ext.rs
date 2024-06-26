ab88cab7cdc7de2fb4b0a36fb852e20e750d8fbf
Etienne <e.desbois@hotmail.fr>
2024-06-26 16:16:40 +0200

    Clean and improve bvh nearest_candidates

--- mesh_to_sdf/src/bvh_ext.rs
+++ mesh_to_sdf/src/bvh_ext.rs
@@ -19,43 +19,64 @@ fn box_sdf<V: Point>(p: &V, aabb: &Aabb<f32, 3>) -> f32 {
 }
 
 pub trait AabbExt {
-    fn point_distance<V: Point>(&self, point: &V) -> f32;
-    fn body_diagonal(&self) -> f32;
+    /// Returns the minimum and maximum distances to the box.
+    /// The minimum distance is the distance to the closest point on the box,
+    /// 0 if the point is inside the box.
+    /// The maximum distance is the distance to the furthest point in the box.
+    fn get_min_max_distances<V: Point>(&self, point: &V) -> (f32, f32);
 }
 
 impl AabbExt for Aabb<f32, 3> {
-    /// Returns the signed distance to the aabb.
-    /// See <https://iquilezles.org/articles/distfunctions/>
-    fn point_distance<V: Point>(&self, point: &V) -> f32 {
-        let p = nalgebra::Point3::new(point.x(), point.y(), point.z());
-        let p = p - self.center();
-        let q = p.abs() - self.size() * 0.5;
+    /// Returns the minimum and maximum distances to the box.
+    /// The minimum distance is the distance to the closest point on the box,
+    /// 0 if the point is inside the box.
+    /// The maximum distance is the distance to the furthest point in the box.
+    fn get_min_max_distances<V: Point>(&self, point: &V) -> (f32, f32) {
+        // Convert point to nalgebra point
+        let n_point: nalgebra::OPoint<f32, nalgebra::Const<3>> =
+            nalgebra::Point3::new(point.x(), point.y(), point.z());
 
-        // lhs is the outside part of the box
-        let lhs = nalgebra::Vector3::new(q.x.max(0.0), q.y.max(0.0), q.z.max(0.0));
-        // rhs is the inside part of the box
-        let rhs = q.max().min(0.0);
-        (lhs + nalgebra::Vector3::from_element(rhs)).norm()
-    }
+        let half_size = self.size() * 0.5;
+        let center = self.center();
 
-    /// Returns the body diagonal of the aabb.
-    fn body_diagonal(&self) -> f32 {
-        self.size().norm()
+        let delta = n_point - center;
+
+        // See <https://iquilezles.org/articles/distfunctions/>
+        let q = delta.abs() - half_size;
+        let min_dist = q.map(|x| x.max(0.0)).norm();
+
+        // The signum helps to determine the furthest point
+        let signum = delta
+            // Invert the signum to get the furthest vertex
+            .map(|x| -x.signum())
+            // Make sure we're always on a vertex and not on a face if the point is aligned with the box
+            .map(|x| if x != 0.0 { x } else { 1.0 });
+
+        let furthest = center + signum.component_mul(&half_size);
+        let max_dist = (n_point - furthest).norm();
+
+        (min_dist, max_dist)
     }
 }
 
 pub trait BvhDistance<V: Point> {
-    fn traverse_distance(&self, origin: &V) -> Vec<usize>
+    /// Traverses the [`Bvh`].
+    /// Returns a subset of `shapes` which are candidates for being the closest to `point`.
+    ///
+    fn nearest_candidates(&self, origin: &V) -> Vec<usize>
     where
         Self: std::marker::Sized;
 }
 
 impl<V: Point> BvhDistance<V> for Bvh<f32, 3> {
-    fn traverse_distance(&self, origin: &V) -> Vec<usize> {
+    /// Traverses the [`Bvh`].
+    /// Returns a subset of `shapes` which are candidates for being the closest to `point`.
+    ///
+    fn nearest_candidates(&self, origin: &V) -> Vec<usize> {
         let mut indices = Vec::new();
         let mut best_min_distance = f32::MAX;
         let mut best_max_distance = f32::MAX;
-        BvhNode::traverse_recursive_distance(
+        BvhNode::nearest_candidates_recursive(
             &self.nodes,
             0,
             origin,
@@ -72,7 +93,10 @@ impl<V: Point> BvhDistance<V> for Bvh<f32, 3> {
     }
 }
 pub trait BvhTraverseDistance<V: Point> {
-    fn traverse_recursive_distance(
+    /// Traverses the [`Bvh`] recursively and returns all shapes whose [`Aabb`] countains
+    /// a candidate shape for being the nearest to the given point.
+    ///
+    fn nearest_candidates_recursive(
         nodes: &[Self],
         node_index: usize,
         origin: &V,
@@ -84,7 +108,10 @@ pub trait BvhTraverseDistance<V: Point> {
 }
 
 impl<V: Point> BvhTraverseDistance<V> for BvhNode<f32, 3> {
-    fn traverse_recursive_distance(
+    /// Traverses the [`Bvh`] recursively and returns all shapes whose [`Aabb`] countains
+    /// a candidate shape for being the nearest to the given point.
+    ///
+    fn nearest_candidates_recursive(
         nodes: &[Self],
         node_index: usize,
         origin: &V,
@@ -100,91 +127,75 @@ impl<V: Point> BvhTraverseDistance<V> for BvhNode<f32, 3> {
                 child_r_index,
                 ..
             } => {
-                let l_dist = child_l_aabb.point_distance(origin);
-                let r_dist = child_r_aabb.point_distance(origin);
+                // Compute min/max dists for both children
+                let [child_l_dists, child_r_dists] =
+                    [child_l_aabb, child_r_aabb].map(|aabb| aabb.get_min_max_distances(origin));
 
-                let l_min_dist = l_dist.max(0.0);
-                let r_min_dist = r_dist.max(0.0);
+                // Update best max distance before traversing children to avoid unnecessary traversals
+                // where right node prunes left node.
+                *best_max_distance = best_max_distance.min(child_l_dists.1.min(child_r_dists.1));
 
-                // if l_dist is positive, we add the dist to aabb to the body diagonal
-                // if it is negative, we still add it and it will be a subtraction since it is negative.
-                // in both cases it keeps the triangle inequality.
-                let l_max_dist = l_dist + child_l_aabb.body_diagonal();
-                let r_max_dist = r_dist + child_r_aabb.body_diagonal();
+                // Traverse children
+                for ((dist_min, dist_max), index) in [
+                    (child_l_dists, child_l_index),
+                    (child_r_dists, child_r_index),
+                ] {
+                    // Node is better by a margin.
+                    if dist_max <= *best_min_distance {
+                        indices.clear();
+                    }
 
-                if !indices.is_empty() && l_max_dist < *best_min_distance {
-                    // left node is better by a margin
-                    indices.clear();
-                }
-
-                if !indices.is_empty() && r_max_dist < *best_min_distance {
-                    // right node is better by a margin
-                    indices.clear();
-                }
-
-                *best_max_distance = best_max_distance.min(l_max_dist.min(r_max_dist));
-
-                if l_min_dist <= *best_max_distance {
-                    Self::traverse_recursive_distance(
-                        nodes,
-                        child_l_index,
-                        origin,
-                        indices,
-                        best_min_distance,
-                        best_max_distance,
-                    );
-                }
-
-                if r_min_dist <= *best_max_distance {
-                    Self::traverse_recursive_distance(
-                        nodes,
-                        child_r_index,
-                        origin,
-                        indices,
-                        best_min_distance,
-                        best_max_distance,
-                    );
+                    // Node might contain a candidate
+                    if dist_min <= *best_max_distance {
+                        Self::nearest_candidates_recursive(
+                            nodes,
+                            index,
+                            origin,
+                            indices,
+                            best_min_distance,
+                            best_max_distance,
+                        );
+                    }
                 }
             }
             BvhNode::Leaf {
                 shape_index,
                 parent_index,
             } => {
+                // Try to compute bounding box via parent node to update best_min/max_distances
                 let parent_node = &nodes[parent_index];
-                if let BvhNode::Node {
+                let (min_dist, max_dist) = if let BvhNode::Node {
                     ref child_l_aabb,
                     child_l_index,
                     ref child_r_aabb,
-                    child_r_index,
                     ..
                 } = parent_node
                 {
-                    let mut min_dist = f32::MAX;
-                    let mut max_dist = f32::MAX;
-                    if *child_l_index == node_index {
-                        let l_dist = child_l_aabb.point_distance(origin);
-                        min_dist = l_dist.max(0.0);
-                        max_dist = l_dist + child_l_aabb.body_diagonal();
-                    }
-                    if *child_r_index == node_index {
-                        let r_dist = child_r_aabb.point_distance(origin);
-                        min_dist = r_dist.max(0.0);
-                        max_dist = r_dist + child_r_aabb.body_diagonal();
-                    }
+                    // We found a parent node, we can compute the min/max distances for the leaf shape.
+                    let aabb = if *child_l_index == node_index {
+                        child_l_aabb
+                    } else {
+                        child_r_aabb
+                    };
+                    let (min_dist, max_dist) = aabb.get_min_max_distances(origin);
 
                     if !indices.is_empty() && max_dist < *best_min_distance {
-                        // left node is better by a margin
+                        // Node is better by a margin
                         indices.clear();
                     }
+
+                    // Also update min_dist here since we have a credible (small) bounding box.
                     *best_min_distance = best_min_distance.min(min_dist);
                     *best_max_distance = best_max_distance.min(max_dist);
 
-                    // we reached a leaf, we add it to the list of indices since it is a potential candidate
-                    indices.push((shape_index, min_dist, max_dist));
+                    (min_dist, max_dist)
                 } else {
                     // The parent is a leaf if the tree is a single node (ie there is only one shape in the tree).
-                    indices.push((shape_index, *best_min_distance, *best_max_distance));
-                }
+                    (*best_min_distance, *best_max_distance)
+                };
+
+                // we reached a leaf, we add it to the list of indices since it is a potential candidate
+                indices.push((shape_index, min_dist, max_dist));
             }
         }
     }
